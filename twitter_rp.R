af6d136253669e41d7c2220f40a259dacb8916e3
Baobao Zhang <baobao.zhang@yale.edu>
2014-07-08 09:31:56 -0400

    Update twitter_rp.R

--- twitter_rp.R
+++ twitter_rp.R
@@ -16,8 +16,8 @@
 # requestURL <- "https://api.twitter.com/oauth/request_token"
 # accessURL <- "https://api.twitter.com/oauth/access_token"
 # authURL <- "https://api.twitter.com/oauth/authorize"
-# consumerKey <- "vnGm03KIVHKCUEqwG9K1heTnd"
-# consumerSecret <- "1M5Inka8XifjGbkZYGsBTnHpsod4ba9DM0LVK03EW4MERVgd7F"
+# consumerKey <- "YOUR CONSUMER KEY"
+# consumerSecret <- "YOUR CONSUMER SECRET KEY"
 # my_oauth <- OAuthFactory$new(consumerKey = consumerKey, consumerSecret = consumerSecret, 
 #                              requestURL = requestURL, accessURL = accessURL, authURL = authURL)
 # my_oauth$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
