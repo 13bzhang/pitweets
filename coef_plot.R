c3fb350248862d11ab31f4f2644f63f2d8d1f451
Baobao Zhang <baobao.zhang@yale.edu>
2014-07-17 00:56:51 -0400

    Update coef_plot.R

--- coef_plot.R
+++ coef_plot.R
@@ -1,54 +1,22 @@
-cd <- as.data.frame(rbind(lm.gdp, lm.religion, lm.language,
-      lm.ally, lm.trade, lm.col, lm.invest))
-names(cd) <- c("mean","se")
-cd$placebo <- c(rep("C: GDP per Capita",5), 
-                rep("D: Likelihood of Being Mostly Christian",5),
-                rep("E: Percent of Population Speaks English",5),
-                rep("F: Likelihood of Military Alliance with the U.S.*",5), 
-                rep("G: Level of Trade with the U.S.*",5), 
-                rep("H: Likelihood of Being U.S. Ally in Iraq War**",5),
-                rep("I: Level of Investment in U.S. Businesses**",5))
-cd$v <- c(rep(c("Basic","Controls","ENE1",
-                "ENE2", "Embedded Natural Experiment"),7))
-cd$ord <- seq(1:5)
-cd$v2 <- factor(cd$v, levels=levels(factor(cd$v)))
-cd$v3 <- factor(cd$v, levels=rev(levels(factor(cd$v))))
-#cd <- cd[with(cd, order(cd$ord)),]
-#cd$v <- factor(cd$v, as.character(cd$v), ordered=T)
-# plab <- c("A.1: Countries Mentioned -- Longitude",
-#           "A.2: Countries Mentioned -- Latitude",
-#           "B.1: Most Plausible Region -- Middle E. and N. Africa",
-#           "B.2; Most Plausible Region -- East Asia",
-#           "C: GDP per Capita", 
-#           "D: Likelihood of Being Mostly Christian",
-#           "E: Percent of Population Speaks English",
-#           "F: Likelihood of Military Alliance with the U.S.",
-#           "G: Trade with the U.S.",
-#           "H: Likelihood of Being U.S. Ally in Iraq War",
-#           "I: Investment in U.S. Businesses")
-f <- ggplot(cd[cd$v!="ENE1" & cd$v!="ENE2",], 
-            aes(x=mean,y=v3,shape=v2,color=v2))
+# summary results:
+cd <- as.data.frame(matrix(NA,3,3))
+names(cd) <- c("mean","se","measure")
+cd$mean <- c(4,2,3)
+cd$se <- c(2,1,2)
+cd$measure <- as.factor(c("C1","C2","C3"))
+# make the graph
+library(ggplot2)
+f <- ggplot(cd, 
+            aes(x=mean,y=measure,shape=measure,color=measure))
 f <- f+geom_vline(xintercept=0, linetype="longdash")+
-  geom_errorbarh(aes(xmax =  mean + 2.576*se, 
+  geom_errorbarh(aes(xmax =  mean + 2.576*se, #99 confidence interval
                      xmin = mean - 2.576*se),
                  size=0.6, height=0) +
-  geom_errorbarh(aes(xmax =  mean + 1.96*se, 
+  geom_errorbarh(aes(xmax =  mean + 1.96*se, #95 confience interval
                      xmin = mean - 1.96*se),
                  size=1.0, height=0)+
   geom_point(stat="identity",size=3.5,fill="white")+
-  scale_color_manual(name="Vingette Type",
+  scale_color_manual(name="Experiment Type",
                      values=c("firebrick3","forestgreen","dodgerblue3"))+
-  scale_shape_manual(name="Vingette Type",values=c(21,22,23))
-f + facet_wrap( ~ placebo, ncol=1)+
-  theme_bb()+
-  xlab("Standardized Difference (Dem-NonDem)")+
-  ylab("")+scale_y_discrete(breaks=NULL) + 
-  ggtitle("Placebo Tests for Confounding")
-ggsave("images/coef_plot_main.pdf", height=7, width=5)
-f + facet_wrap( ~ placebo, ncol=1)+
-  theme_bbtop()+
-  xlab("Standardized Difference (Dem-NonDem)")+
-  ylab("")+scale_y_discrete(breaks=NULL) + 
-  ggtitle("Placebo Tests for Confounding")
-ggsave("images/coef_plot_main_top.pdf", height=7, width=5.5)
-ggsave("images/coef_plot_main_poster.pdf", height=6, width=12)
+  scale_shape_manual(name="Vingette Type",values=c(21,22,23))+
+  xlab("Coeffient")+ylab("")
